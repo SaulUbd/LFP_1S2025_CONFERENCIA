ca5f46f14ed4a8362b243242d17f9d8e1cbbc2cf
Saul Castellanos <saulcastel018@gmail.com>
2025-04-09 00:47:27 -0600

    actualizar script principal

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ const filePath = process.argv[2];
 const source = readFileSync(filePath, 'utf-8');
 const scanner = new Scanner(source);
 const scanResult = scanner.scan();
+console.log('===Resultados de lexer===');
 console.log(scanResult);
+if (scanResult.errors.length != 0) {
+    console.error('>>> Hubo errores en el escaneo');
+    process.exit(1);
+}
 const parser = new Parser(scanResult.tokens);
-const tree = parser.parse();
-console.log(tree);
+const parseResult = parser.parse();
+console.log('===Resultados de parser===');
+console.log(parseResult);
+if (!parseResult.parseTree) {
+    console.error('>>> Hubo errores en el parseo');
+    process.exit(1);
+}
